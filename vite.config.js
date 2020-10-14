3a02c65936c224b134f4ce9901db433aecc566e2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-15 03:02:40 +0900

    :wrench: Add path alias

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 // @ts-check
 const preactRefresh = require('@prefresh/vite')
 
+const pathAliasMap = {
+  '@/': '/src/',
+}
+
 /**
  * @type { import('vite').UserConfig }
  */
 const config = {
+  resolvers: [
+    {
+      alias(path) {
+        for (const [slug, res] of Object.entries(pathAliasMap)) {
+          if (path.startsWith(slug)) {
+            return path.replace(slug, res)
+          }
+        }
+      },
+    },
+  ],
   jsx: {
     factory: 'h',
     fragment: 'Fragment',
