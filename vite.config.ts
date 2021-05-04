6964756f96144242c7886ba4497ce0a822b96e12
Furkan Acar <furkan@acar.digital>
2021-05-05 02:30:39 +0300

    Added eslint plugins, linted.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-import preactRefresh from '@prefresh/vite'
-import { resolve } from 'path'
-import type { UserConfig } from 'vite'
+import preactRefresh from '@prefresh/vite';
+import { resolve } from 'path';
+import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
   },
-
   plugins: [preactRefresh()],
-
   resolve: {
     alias: [{ find: '@', replacement: resolve('src') }],
   },
-}
+};
 
-export default config
+export default config;
